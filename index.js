0b8a0fee47a5fda82ba1f68b49e9f49f50390e15
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-14 15:29:07 +0100

    15. Refactor, or update, your addTwoNumbers function to remove the return keyword and the curly braces {}. Your addTwoNumbers function should instead use an implicit return.Open up the console to make sure that you are still getting the correct output.

--- index.js
+++ index.js
@@ -96,7 +96,6 @@ const printMessage = (org) => {
 };
 printMessage("freeCodeCamp");
 
-const addTwoNumbers = (num1, num2) => {
-  return num1 + num2;
-};
+const addTwoNumbers = (num1, num2) => num1 + num2;
+
 console.log(addTwoNumbers(3, 4));
