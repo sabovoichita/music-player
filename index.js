a4c55d4b47fff0a6005212e4857a258f17977e76
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-14 15:17:42 +0100

    11. Create a new named arrow function called printGreeting. Inside the body of that function use the console.log() method to print the string "Hello there!".

--- index.js
+++ index.js
@@ -80,9 +80,12 @@ const allSongs = [
 ];
 
 const audio = new Audio();
-
 let userData = {
   songs: [...allSongs],
   currentSong: null,
   songCurrentTime: 0,
+};
+
+const printGreeting = () => {
+  console.log("Hello there!");
 };
