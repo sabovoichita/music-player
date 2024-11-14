bd34c00cf203c53f8e4ce073d7c51502198c8130
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-14 14:54:26 +0100

    1. Start by accessing the #playlist-songs, #play, and #pause elements with the getElementById() method. Assign them to variables playlistSongs, playButton and pauseButton respectively.

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log("Welcome!");
+const playlistSongs = document.getElementById("playlist-songs");
+const playButton = document.getElementById("play");
+const pauseButton = document.getElementById("pause");
