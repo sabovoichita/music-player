6600c22e35615637de1a27ead5ba309e2e30e6b4
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-14 15:41:33 +0100

    22. nside the button element, create two more span elements.
    The first span element should have a class of playlist-song-artist. In between the span tags, add ${song.artist}. The second span element should have a class of playlist-song-duration. In between the span tags, add ${song.duration}.

--- index.js
+++ index.js
@@ -104,5 +104,7 @@ const renderSongs = (array) => {};
 const songsHTML = array.map((song) => {
   return `<li id="song-${song.id}" class="playlist-song"></li>
           <button class="playlist-song-info"><span class="playlist-song-title">${song.title}</span></button>
+          <button class="playlist-song-info"><span class="playlist-song-artist">${song.artist}</span></button>
+          <button class="playlist-song-info"><span class="playlist-song-duration">${song.duration}</span></button>
   `;
 });
