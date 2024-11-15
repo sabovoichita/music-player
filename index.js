68c9bd9458e748bce79b4b351c21f4ff6bb9c347
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-15 09:32:51 +0100

    39. Within the else block, set the currentTime property of the audio object to the value stored in userData?.songCurrentTime.

--- index.js
+++ index.js
@@ -107,6 +107,8 @@ const playSong = (id) => {
   audio.title = song.title;
   if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
     audio.currentTime = 0;
+  } else {
+    audio.currentTime = userData?.songCurrentTime;
   }
 };
 console.log(playSong(1));
