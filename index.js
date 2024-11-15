92f33105dae8f758ecdfea7dd0450d71f0e3854a
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-15 11:46:59 +0100

    44. Add an else block. Inside the else block, call the playSong function with the id of the currently playing song as an argument. This ensures that the currently playing song will continue to play when the play button is clicked.

--- index.js
+++ index.js
@@ -118,6 +118,8 @@ const playSong = (id) => {
 playButton.addEventListener("click", () => {
   if (userData?.currentSong === null) {
     playSong(userData?.songs[0].id);
+  } else {
+    playSong(userData?.currentSong.id);
   }
 });
 
