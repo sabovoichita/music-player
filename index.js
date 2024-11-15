62bc18a23a750c6100e7e846f50f3b788bac2871
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-15 12:16:49 +0100

    50. Before you start working on playing the next and previous song, you need to get the index of each song in the songs property of userData. Start by creating an arrow function called getCurrentSongIndex.

--- index.js
+++ index.js
@@ -117,8 +117,6 @@ const playSong = (id) => {
   audio.play();
 };
 
-pauseButton.addEventListener("click", pauseSong);
-
 const pauseSong = () => {
   userData.songCurrentTime = audio.currentTime;
   playButton.classList.remove("playing");
@@ -146,6 +144,8 @@ const renderSongs = (array) => {
   playlistSongs.innerHTML = songsHTML;
 };
 
+const getCurrentSongIndex = () => {};
+
 playButton.addEventListener("click", () => {
   if (userData?.currentSong === null) {
     playSong(userData?.songs[0].id);
@@ -153,6 +153,8 @@ playButton.addEventListener("click", () => {
     playSong(userData?.currentSong.id);
   }
 });
+
+pauseButton.addEventListener("click", pauseSong);
 
 const sortSongs = () => {
   userData?.songs.sort((a, b) => {
