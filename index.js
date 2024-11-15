a88eefe0b1899ffc4cc9b93110ebbaaf59d9cbb4
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-15 12:12:51 +0100

    48. Use classList and remove() method to remove the .playing class from the playButton, since the song will be paused at this point. To finally pause the song, use the pause() method on the audio variable. pause() is a method of the Web Audio API for pausing music files.

--- index.js
+++ index.js
@@ -119,6 +119,8 @@ const playSong = (id) => {
 
 const pauseSong = () => {
   userData.songCurrentTime = audio.currentTime;
+  playButton.classList.remove("playing");
+  audio.pause();
 };
 
 const renderSongs = (array) => {
