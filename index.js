8cff3f5e29dbc73a377cde33b6747a4b53fbe1f5
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-15 15:14:09 +0100

    79. Call the renderSongs function and pass in userData?.songs as an argument. Also, call the pauseSong, setPlayerDisplay, and setPlayButtonAccessibleText functions.

--- index.js
+++ index.js
@@ -151,6 +151,10 @@ const shuffle = () => {
   userData?.songs.sort(() => Math.random() - 0.5);
   userData.currentSong = null;
   userData.songCurrentTime = 0;
+  renderSongs(userData?.songs);
+  pauseSong();
+  setPlayerDisplay();
+  setPlayButtonAccessibleText();
 };
 
 const setPlayerDisplay = () => {
